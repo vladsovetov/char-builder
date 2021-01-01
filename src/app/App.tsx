7af3dcc7058d4195a831aed446ac3dab8bd9f457
Vlad Sovetov <vlad.sovetov@gmail.com>
2021-01-02 01:00:14 +0200

    chore: Init setup

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { AdminPage } from "app/pages/Admin";
+import "./App.css";
+
+const App = () => (
+  <Switch>
+    <Route exact path="/admin" component={AdminPage} />
+  </Switch>
+);
+
+export default App;
