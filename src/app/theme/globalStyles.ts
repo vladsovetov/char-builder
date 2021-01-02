423403d323487bfa6d96c51003b96058bed6da49
Vlad Sovetov <vlad.sovetov@gmail.com>
2021-01-02 12:20:20 +0200

    chore: MYP-28 Added styled components setup

---
+++ src/app/theme/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+    height: 100%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`
